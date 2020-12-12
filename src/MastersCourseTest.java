32f84dacae2d1fd41bda2417186b25229ca0e7ef
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2020-12-13 08:00:18 +0900

    rotateFace(), rotateCW(), rotateCCW() 메소드 작성
    들여쓰기 3단계를 넘지않기 위해 시계/반시계 방향 90도 회전을 각각 rotateCW(), rotateCCW() 메소드로 분리

--- src/MastersCourseTest.java
+++ src/MastersCourseTest.java
@@ -12,31 +12,7 @@ class RubiksCube {
     private int countCommands = 0;
 
     public void print() {
-        for(String[] i : this.rubiksCube[0]) {
-            System.out.print("               ");
-            for(String j : i) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 1; j < 5; j++) {
-                System.out.print(this.rubiksCube[j][i][0] + " " + this.rubiksCube[j][i][1] + " " + this.rubiksCube[j][i][2] + "     ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        for(String[] i : this.rubiksCube[5]) {
-            System.out.print("               ");
-            for(String j : i) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+        // TO DO: 큐브의 면을 각각의 방향에 맞게 회전해서 출력
     }
 
     public void play() {
@@ -59,7 +35,7 @@ class RubiksCube {
                 return;
             }
 
-            // 시계 방향 = 1, 반시계 방향 = -1, 180도 회전 = 2로 설정
+            // 시계 방향 90도 회전 = 1, 반시계 방향 90도 회전 = -1, 180도 회전 = 2로 설정
             int direction = 1;
             if(i != commands.length()-1 && commands.charAt(i+1) == '\'')
                 direction = -1;
@@ -94,7 +70,7 @@ class RubiksCube {
                 continue;
             }
 
-            // 반시계 방향인 경우 ', 180도 회전인 경우 2를 출력
+            // 반시계 방향 90도 회전인 경우 ', 180도 회전인 경우 2를 출력
             if(direction == 1)
                 System.out.println();
             else if(direction == -1)
@@ -110,7 +86,54 @@ class RubiksCube {
         if(direction == 0)
             return;
 
-        this.rotateFace(targetFace, topSide, leftSide, rightSide, bottomSide, Math.abs(direction) - 1);
+        String[] swapSide = new String[3];
+
+        if(direction > 1) {
+            this.rotateCW(targetFace);
+
+            for(int i = 0; i < 3; i++)
+                swapSide[i] = this.rubiksCube[topSide][0][i];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[topSide][0][i] = this.rubiksCube[bottomSide][2][2-i];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[bottomSide][2][i] = this.rubiksCube[leftSide][i][0];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[leftSide][i][0] = this.rubiksCube[rightSide][2-i][2];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[rightSide][i][2] = swapSide[i];
+
+            // 180도 회전인 경우 시계 방향 90도 회전을 한 번 더 실행
+            this.rotateFace(targetFace, topSide, leftSide, rightSide, bottomSide, direction - 1);
+        } else {
+            this.rotateCCW(targetFace);
+
+            for(int i = 0; i < 3; i++)
+                swapSide[i] = this.rubiksCube[topSide][0][i];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[topSide][0][i] = this.rubiksCube[rightSide][i][2];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[rightSide][i][2] = this.rubiksCube[leftSide][2-i][0];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[leftSide][i][0] = this.rubiksCube[bottomSide][2][i];
+            for(int i = 0; i < 3; i++)
+                this.rubiksCube[bottomSide][2][i] = swapSide[2-i];
+        }
+    }
+
+    private void rotateCW(int targetFace) {
+        String[][] swapFace = this.rubiksCube[targetFace];
+
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                this.rubiksCube[targetFace][j][2-i] = swapFace[i][j];
+    }
+    
+    private void rotateCCW(int targetFace) {
+        String[][] swapFace = this.rubiksCube[targetFace];
+
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
+                this.rubiksCube[targetFace][2-j][i] = swapFace[i][j];
     }
 }
 
