0563ff41877e008d5559bad753e936e81ffac228
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2020-12-13 05:33:32 +0900

    기본 형태 작성
    2단계에서 작성한 코드를 응용하여 3단계의 기본 형태 작성
    print() 메소드를 정육면체 큐브의 구조를 출력하도록 수정

--- src/MastersCourseTest.java
+++ src/MastersCourseTest.java
@@ -1,14 +1,38 @@
 import java.util.Scanner;
 
-class PlaneCube {
+class RubiksCube {
 
-    private String[][] planeCube = {{"R", "R", "W"}, {"G", "C", "W"}, {"G", "B", "B"}};
+    private String[][][] rubiksCube = {{{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}},
+            {{"W", "W", "W"}, {"W", "W", "W"}, {"W", "W", "W"}},
+            {{"O", "O", "O"}, {"O", "O", "O"}, {"O", "O", "O"}},
+            {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}},
+            {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}},
+            {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}}};
     private boolean isEnd = false;  // 종료 판정용 flag
 
     public void print() {
-        for (String[] i : this.planeCube) {
-            for (String j : i)
+        for(String[] i : this.rubiksCube[0]) {
+            System.out.print("               ");
+            for(String j : i) {
                 System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 1; j < 5; j++) {
+                System.out.print(this.rubiksCube[j][i][0] + " " + this.rubiksCube[j][i][1] + " " + this.rubiksCube[j][i][2] + "     ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(String[] i : this.rubiksCube[5]) {
+            System.out.print("               ");
+            for(String j : i) {
+                System.out.print(j + " ");
+            }
             System.out.println();
         }
         System.out.println();
@@ -26,82 +50,14 @@ class PlaneCube {
     }
 
     private void runCommands(String commands) {
-        for(int i = 0; i < commands.length(); i++) {
-            if(commands.charAt(i) == 'Q') {
-                this.isEnd = true;
-                System.out.println("Bye~");
-                return;
-            }
 
-            // U(왼쪽), R(위쪽)을 정방향(true)로 설정, 역방향(')인 경우 false
-            // B, L 명령어는 U, R과 방향이 반대이므로 !(not)으로 반전하여 넘겨줌
-            boolean direction = true;
-            if(i != commands.length()-1 && commands.charAt(i+1) == '\'')
-                direction = false;
-
-            if(commands.charAt(i) == 'U') {
-                System.out.print("U");
-                pushLineX(0, direction);
-            } else if(commands.charAt(i) == 'B') {
-                System.out.print("B");
-                pushLineX(2, !direction);
-            } else if(commands.charAt(i) == 'R') {
-                System.out.print("R");
-                pushLineY(2, direction);
-            } else if(commands.charAt(i) == 'L') {
-                System.out.print("L");
-                pushLineY(0, !direction);
-            } else {
-                continue;
-            }
-
-            // 역방향인 경우 '를 출력
-            if(direction)
-                System.out.println();
-            else
-                System.out.println("'");
-
-            print();
-        }
-    }
-
-    private void pushLineX(int line, boolean direction) {
-        if(direction) {
-            String swap = planeCube[line][0];
-
-            planeCube[line][0] = planeCube[line][1];
-            planeCube[line][1] = planeCube[line][2];
-            planeCube[line][2] = swap;
-        } else {
-            String swap = planeCube[line][2];
-
-            planeCube[line][2] = planeCube[line][1];
-            planeCube[line][1] = planeCube[line][0];
-            planeCube[line][0] = swap;
-        }
-    }
-
-    private void pushLineY(int line, boolean direction) {
-        if(direction) {
-            String swap = planeCube[0][line];
-
-            planeCube[0][line] = planeCube[1][line];
-            planeCube[1][line] = planeCube[2][line];
-            planeCube[2][line] = swap;
-        } else {
-            String swap = planeCube[2][line];
-
-            planeCube[2][line] = planeCube[1][line];
-            planeCube[1][line] = planeCube[0][line];
-            planeCube[0][line] = swap;
-        }
     }
 }
 
 public class MastersCourseTest {
 
     public static void main(String[] args) {
-        PlaneCube cube = new PlaneCube();
+        RubiksCube cube = new RubiksCube();
         cube.print();
         cube.play();
     }
