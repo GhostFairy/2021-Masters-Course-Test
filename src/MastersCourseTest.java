b33cc810439579185934afae32845ca2fa1bfd9d
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2020-12-13 18:14:11 +0900

    모든 면을 맞췄을 시에 대한 동작 추가 및 구조 수정
    모든 면을 맞췄는지 판정을 위해 초기 상태를 저장하는 내부 변수 originalCubeState 생성
    각 메소드의 길이가 20줄이 넘지 않도록 조정하기 위해 구조 수정

--- src/MastersCourseTest.java
+++ src/MastersCourseTest.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 class RubiksCube {
@@ -8,9 +9,15 @@ class RubiksCube {
                                        {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}},
                                        {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}},
                                        {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}}};
+    private String[][][] originalCubeState = {{{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}},
+                                              {{"W", "W", "W"}, {"W", "W", "W"}, {"W", "W", "W"}},
+                                              {{"O", "O", "O"}, {"O", "O", "O"}, {"O", "O", "O"}},
+                                              {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}},
+                                              {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}},
+                                              {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}}};
     private boolean isEnd = false;  // 종료 판정용 flag
     private int countCommands = 0;
-    private long initTime = System.currentTimeMillis();
+    private final long initTime = System.currentTimeMillis();
 
     public void print() {
         // 큐브의 면을 각각의 방향에 맞게 회전해서 출력
@@ -55,128 +62,132 @@ class RubiksCube {
 
         while(!this.isEnd) {
             System.out.print("CUBE> ");
-            this.runCommands(scan.nextLine(), false);
+            String commands = scan.nextLine();
+
+            for(int i = 0; i < commands.length(); i++) {
+                // 시계 방향 90도 회전 = 1, 반시계 방향 90도 회전 = -1, 180도 회전 = 2로 설정
+                int direction = 1;
+                if(i != commands.length()-1 && commands.charAt(i+1) == '\'')
+                    direction = -1;
+                else if(i != commands.length()-1 && commands.charAt(i+1) == '2')
+                    direction = 2;
+
+                this.runCommands(commands.charAt(i), direction, false);
+            }
         }
         scan.close();
+        this.endPlay();
+    }
 
+    public void shuffle() {
+        String availableCommands = "FRUBLD";
+        int[] availableDirection = {-1, 1, 2};
+
+        for(int i = 0; i < 50; i++) {
+            char command = availableCommands.charAt((int) (Math.random() * 6));
+            int direction = availableDirection[(int) (Math.random() * 3)];
+
+            this.runCommands(command, direction, true);
+        }
+
+        this.countCommands = 0;
+    }
+
+    private void endPlay() {
         long elapsedTime = (System.currentTimeMillis() - this.initTime)/1000;
         System.out.printf("경과시간: %02d:%02d\n", elapsedTime/60, elapsedTime%60);
         System.out.println("조작갯수: " + this.countCommands);
         System.out.println("이용해주셔서 감사합니다. 뚜뚜뚜.");
     }
 
-    public void shuffle() {
-        String availableCommands = "FRUBLD'2";
-        StringBuilder commands = new StringBuilder();
+    private void runCommands(char command, int direction, boolean mute) {
+        if(command == 'Q') {
+            this.isEnd = true; return; }
+        else if(command == 'F')
+            this.rotateFace(2, 0, 5, 3, 1, direction);
+        else if(command == 'R')
+            this.rotateFace(3, 5, 0, 2, 4, direction);
+        else if(command == 'U')
+            this.rotateFace(0, 2, 4, 1, 3, direction);
+        else if(command == 'D')
+            this.rotateFace(5, 3, 1, 4, 2, direction);
+        else if(command == 'L')
+            this.rotateFace(1, 4, 2, 0, 5, direction);
+        else if(command == 'B')
+            this.rotateFace(4, 1, 3, 5, 0, direction);
+        else
+            return;
+        this.countCommands += Math.abs(direction);
+        this.printCommand(command, direction, mute);
 
-        for(int i = 0; i < 100; i++)
-            commands.append(availableCommands.charAt((int)(Math.random()*8)));
-
-        this.runCommands(commands.toString(), true);
-        this.countCommands = 0;
+        if(Objects.deepEquals(this.rubiksCube, this.originalCubeState)) {
+            System.out.println("축하합니다! 모든 면을 맞추셨습니다."); this.isEnd = true;}
     }
 
-    private void runCommands(String commands, boolean mute) {
-        for(int i = 0; i < commands.length(); i++) {
-            if(commands.charAt(i) == 'Q') {
-                this.isEnd = true;
-                return;
-            }
+    private void printCommand(char command, int direction, boolean mute) {
+        // mute 상태일 경우 명령어와 큐브의 상태를 출력하는 아래의 구문을 실행하지 않음
+        if(mute)
+            return;
 
-            // 시계 방향 90도 회전 = 1, 반시계 방향 90도 회전 = -1, 180도 회전 = 2로 설정
-            int direction = 1;
-            if(i != commands.length()-1 && commands.charAt(i+1) == '\'')
-                direction = -1;
-            else if(i != commands.length()-1 && commands.charAt(i+1) == '2')
-                direction = 2;
+        if(direction == 1)
+            System.out.println(command);
+        else if (direction == -1)
+            // 반시계 방향 90도 회전인 경우 '를 추가로 출력
+            System.out.println(command + "'");
+        else
+            // 180도 회전인 경우 2를 추가로 출력
+            System.out.println(command + "2");
 
-            if(commands.charAt(i) == 'F') {
-                this.rotateFace(2, 0, 5, 3, 1, direction);
-                this.countCommands += Math.abs(direction);
-            } else if(commands.charAt(i) == 'R') {
-                this.rotateFace(3, 5, 0, 2, 4, direction);
-                this.countCommands += Math.abs(direction);
-            } else if(commands.charAt(i) == 'U') {
-                this.rotateFace(0, 2, 4, 1, 3, direction);
-                this.countCommands += Math.abs(direction);
-            } else if(commands.charAt(i) == 'D') {
-                this.rotateFace(5, 3, 1, 4, 2, direction);
-                this.countCommands += Math.abs(direction);
-            } else if(commands.charAt(i) == 'L') {
-                this.rotateFace(1, 4, 2, 0, 5, direction);
-                this.countCommands += Math.abs(direction);
-            } else if(commands.charAt(i) == 'B') {
-                this.rotateFace(4, 1, 3, 5, 0, direction);
-                this.countCommands += Math.abs(direction);
-            } else {
-                continue;
-            }
-
-            // mute 상태일 경우 명령어와 큐브의 상태를 출력하는 아래의 구문을 실행하지 않음
-            if(mute)
-                continue;
-
-            if(direction == 1)
-                System.out.println(commands.charAt(i));
-            else
-                // 반시계 방향 90도 회전인 경우 ', 180도 회전인 경우 2를 추가로 출력
-                System.out.println(commands.charAt(i) + "" + commands.charAt(++i));
-            this.print();
-        }
+        this.print();
     }
 
     private void rotateFace(int targetFace, int topSide, int leftSide, int rightSide, int bottomSide, int direction) {
-        if(direction == 0)
-            return;
-
-        String[] swapSide = this.rubiksCube[topSide][0].clone();
-
-        if(direction > 0) {
-            this.rotateCW(targetFace);
-
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[topSide][0][i] = this.rubiksCube[bottomSide][2][2-i];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[bottomSide][2][i] = this.rubiksCube[leftSide][i][0];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[leftSide][i][0] = this.rubiksCube[rightSide][2-i][2];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[rightSide][i][2] = swapSide[i];
-
-            // 180도 회전인 경우 시계 방향 90도 회전을 한 번 더 실행
-            this.rotateFace(targetFace, topSide, leftSide, rightSide, bottomSide, direction - 1);
-        } else {
-            this.rotateCCW(targetFace);
-
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[topSide][0][i] = this.rubiksCube[rightSide][i][2];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[rightSide][i][2] = this.rubiksCube[leftSide][2-i][0];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[leftSide][i][0] = this.rubiksCube[bottomSide][2][i];
-            for(int i = 0; i < 3; i++)
-                this.rubiksCube[bottomSide][2][i] = swapSide[2-i];
+        if(direction == 1)
+            this.rotateCW(targetFace, topSide, leftSide, rightSide, bottomSide);
+        else if(direction == 2) {
+            this.rotateCW(targetFace, topSide, leftSide, rightSide, bottomSide);
+            this.rotateCW(targetFace, topSide, leftSide, rightSide, bottomSide);
         }
+        else
+            this.rotateCCW(targetFace, topSide, leftSide, rightSide, bottomSide);
     }
 
-    private void rotateCW(int targetFace) {
+    private void rotateCW(int targetFace, int topSide, int leftSide, int rightSide, int bottomSide) {
         String[][] swapFace = new String[3][3];
         for(int i = 0; i < 3; i++)
             swapFace[i] = this.rubiksCube[targetFace][i].clone();
+        String[] swapSide = this.rubiksCube[topSide][0].clone();
 
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
                 this.rubiksCube[targetFace][i][j] = swapFace[2-j][i];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[topSide][0][i] = this.rubiksCube[bottomSide][2][2-i];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[bottomSide][2][i] = this.rubiksCube[leftSide][i][0];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[leftSide][i][0] = this.rubiksCube[rightSide][2-i][2];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[rightSide][i][2] = swapSide[i];
     }
     
-    private void rotateCCW(int targetFace) {
+    private void rotateCCW(int targetFace, int topSide, int leftSide, int rightSide, int bottomSide) {
         String[][] swapFace = new String[3][3];
         for(int i = 0; i < 3; i++)
             swapFace[i] = this.rubiksCube[targetFace][i].clone();
+        String[] swapSide = this.rubiksCube[topSide][0].clone();
 
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 3; j++)
                 this.rubiksCube[targetFace][i][j] = swapFace[j][2-i];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[topSide][0][i] = this.rubiksCube[rightSide][i][2];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[rightSide][i][2] = this.rubiksCube[leftSide][2-i][0];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[leftSide][i][0] = this.rubiksCube[bottomSide][2][i];
+        for(int i = 0; i < 3; i++)
+            this.rubiksCube[bottomSide][2][i] = swapSide[2-i];
     }
 }
 
